bc6f12259b0d0e82b6620c3e5aa4c6f44918a6ac
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-08-30 16:28:15 +0100

    feat feed: Add data feed, format posts

--- postApp/ContentView.swift
+++ postApp/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+//struct Response: Codable {
+//    var results: [Result]
+//}
+
+struct Result: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("Posts").font(.title)
+            List(results, id: \.id) { item in
+                VStack(alignment: .leading) {
+                    Text(item.title).font(.headline).padding(.bottom)
+                    Text(item.body)
+                }
+            }
+            .task {
+                await loadData()
+            }
+            
         }
-        .padding()
+                
+        }
+        func loadData() async {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            //data from method tkes URL and returns a Data instance at that URL, belongs to URLSession class
+            // returns a tuple, (data we want + metadata) (data, _) = (data, discard the response)
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode([Result].self, from: data) {
+                results = decodedResponse
+                print(decodedResponse)
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
---
+++ postApp/postsAPI.swift
@@ -0,0 +1,18 @@
+//
+//  postsAPI.swift
+//  postApp
+//
+//  Created by Matt Wilkes on 30/08/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct PostData: Identifiable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+
