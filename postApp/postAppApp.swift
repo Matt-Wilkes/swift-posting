54ad4b0027842e313ef0a0728121e0b593f55cd5
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-08-30 14:39:30 +0100

    Initial Commit

---
+++ postApp/postAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  postAppApp.swift
+//  postApp
+//
+//  Created by Matt Wilkes on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct postAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
